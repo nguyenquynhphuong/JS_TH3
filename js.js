bfebb2516bd32da6ac796bdd526583540c2729c8
unknown <nqphuong010j1@gmail.com>
2022-01-15 17:01:16 +0700

    THJS3

---
+++ js.js
@@ -0,0 +1,13 @@
+
+let a = prompt('Nhập số a');
+let b = prompt('Nhập số b');
+let check = a % b;
+if(check === 0) {
+    document.write("a là bội số của b");
+}
+else{
+    document.write("a không là bội số của b");
+}
+
+
+
